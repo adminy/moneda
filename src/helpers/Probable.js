636e61530c3b184590528af319e9b84b006cdacb
adminy <marin.bivol@dcu.ie>
2021-08-01 17:28:47 +0100

    Version bump rpc package, fix bug

--- src/helpers/Probable.js
+++ src/helpers/Probable.js
@@ -1,24 +1,25 @@
 'use strict'
 
 const sum = list => list.reduce((a, b) => a + b, 0)
-module.exports = {
-  calc: values => {
-    let itemIndex
-    while (values.length > 2) {
-      const avg = sum(values) / values.length
-      const min = Math.min(...values)
-      const max = Math.max(...values)
-      if ((min + max) / 2 === avg) return avg
-      let maxDeviation = 0
-      values.forEach((value, i) => {
-        const deviation = Math.abs(value - avg)
-        if (deviation > maxDeviation) {
-          maxDeviation = deviation
-          itemIndex = i
-        }
-      })
-      values.splice(itemIndex, 1)
-    }
-    return parseInt(sum(values) / values.length)
+
+const calc = values => {
+  let itemIndex
+  while (values.length > 2) {
+    const avg = sum(values) / values.length
+    const min = Math.min(...values)
+    const max = Math.max(...values)
+    if ((min + max) / 2 === avg) return avg
+    let maxDeviation = 0
+    values.forEach((value, i) => {
+      const deviation = Math.abs(value - avg)
+      if (deviation > maxDeviation) {
+        maxDeviation = deviation
+        itemIndex = i
+      }
+    })
+    values.splice(itemIndex, 1)
   }
-}()
+  return parseInt(sum(values) / values.length)
+}
+
+module.exports = { calc }
